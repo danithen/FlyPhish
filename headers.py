9d35adbc72938d51459ba40af1444bc1efaac201
Dan <168593361+danithen@users.noreply.github.com>
2024-11-05 11:32:37 -0600

    Update headers.py

---
+++ headers.py
@@ -0,0 +1,61 @@
+import win32com.client
+import sqlite3
+import os
+from sqlite_utils import Database
+
+#username=input("Username:")
+#passwd=input("Passwd:")
+
+#init the flyphish.db database
+#db = Database(sqlite3.connect("flyphish.db"))
+#db["email_headers"].create({
+#   "id": str,
+#    "Return-Path": str,
+#    "From": str,
+#   "Authentication-Results": str,
+#    "Subject": str,
+#    "To": str
+#}, pk="id", if_not_exists=True)
+
+desired_headers= ['Return-Path','From','Authentication-Results','Reply-To', 'Subject', 'To', 'Date']
+outlook_app = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+inbox = outlook_app.GetDefaultFolder(6)
+messages=inbox.Items
+unread_messages = messages.Restrict("[Unread] = True")
+
+print(f"Total Inbox Items: {inbox.Items.Count}")
+header_lines = ('')
+output_dir = r
+for message in unread_messages:
+    #print("start")
+    #print (f"Sender: {message.Sender}")
+    headers = message.PropertyAccessor.GetProperty("http://schemas.microsoft.com/mapi/proptag/0x007D001E")
+    header_lines = headers.split("\n")
+    formatted_headers = {}
+    
+    for attachment in message.Attachments:
+        attachment.SaveAsFile(os.path.join(output_dir, attachment.FileName))
+        
+    for line in header_lines:
+        if ': ' in line:
+            key, value = line.split(': ', 1)
+            key = key.strip()
+            value = value.strip()
+            if key in desired_headers:
+                formatted_headers[key] = value
+
+    for key, value in formatted_headers.items():
+        print(f"{key}: {value}")
+    
+
+    #email_data = {
+    #    "id": message.EntryID,  # Use EntryID as a unique identifier
+    #    "Return-Path": formatted_headers.get('Return-Path', ''),
+    #    "From": formatted_headers.get('From', ''),
+    #    "Authentication-Results": formatted_headers.get('Authentication-Results', ''),
+    #    "Subject": formatted_headers.get('Subject', ''),
+    #    "To": formatted_headers.get('To', '')
+    #}
+#    db['email_headers'].insert(email_data)
+#db.connection.commit()
+#db.connection.close()
